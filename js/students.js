6aa9ea23bd6e820befef8205e91aeda9120d6545
erpsru <prepwithraj@gmail.com>
2026-02-09 12:41:55 +0530

    Update students.js

--- js/students.js
+++ js/students.js
@@ -1,101 +1,135 @@
 const students = {
-"36852": {
-    name: "ARCHIT KUMAR",
-    father: "BRHAM PAL",
-    mother: "REKHA RANI",
-    session: "2025-26",
-    course: "DIPLOMA IN ENGG",
-    stream: "",
-    category: "GENERAL",
-    gender: "MALE",
-    year: "2",
 
-    address: "VILL RUMASA, TEKURU",
-    district: "FIROZABAD",
-    state: "UTTAR PRADESH",
-    country: "INDIA",
+    /* =========================
+       ARCHIT KUMAR
+    ========================== */
+    "36852": {
+        name: "ARCHIT KUMAR",
+        father: "BRHAM PAL",
+        mother: "REKHA RANI",
 
-    photo: "images/archit.jpg",
+        session: "2025-26",
+        course: "DIPLOMA IN ENGG",
+        stream: "",
 
-    qualification: [
-        {
-            exam: "HIGH SCHOOL",
-            year: "2013",
-            board: "UP BOARD",
-            marks: "64",
-            subjects: "HINDI, ENG, MATHS, SCIENCE"
-        },
-        {
-            exam: "ITI",
-            year: "2017",
-            board: "NCVT",
-            marks: "-",
-            subjects: "MACHINIST / GRINDER"
-        }
-    ]
-},
+        category: "GENERAL",
+        gender: "MALE",
+        year: "2",
 
-"37652": {
-    name: "ABHISHEK PAIL WAR",
-    father: "MAHAVEER SINGH",
-    mother: "-",
-    session: "2024-25",
-    course: "MASTER OF BUSINESS ADMINISTRATION",
-    stream: "MARKETING & HR",
-    category: "OBC",
-    gender: "MALE",
-    year: "1",
+        address: "VILL RUMASA, TEKURU",
+        district: "FIROZABAD",
+        state: "UTTAR PRADESH",
+        country: "INDIA",
 
-    address: "DIGRAUTA, AGRA",
-    district: "AGRA",
-    state: "UTTAR PRADESH",
-    country: "INDIA",
+        photo: "images/archit.jpg",
 
-    photo: "images/abhishek.jpg",
+        qualification: [
+            {
+                exam: "HIGH SCHOOL",
+                year: "2013",
+                board: "UP BOARD",
+                marks: "64",
+                subjects: "HINDI, ENG, MATHS, SCIENCE"
+            },
+            {
+                exam: "ITI",
+                year: "2017",
+                board: "NCVT",
+                marks: "-",
+                subjects: "MACHINIST / GRINDER"
+            }
+        ]
+    },
 
-    qualification: [
-        {
-            exam: "10th",
-            year: "2012",
-            board: "Raj Board",
-            marks: "50",
-            subjects: "Hindi, English, Maths, Science"
-        },
-        {
-            exam: "12th",
-            year: "2015",
-            board: "UP BOARD",
-            marks: "51",
-            subjects: "Hindi, English, Maths"
-        },
-        {
-            exam: "Graduation",
-            year: "2019",
-            board: "SVSU Meerut",
-            marks: "67",
-            subjects: "Maths / Physics"
-        }
-    ]
-};
 
-"SRU220740087": {
-    name: "RUPESH KUMAR SINGH",
-    father: "SANTOSH KUMAR SINGH",
-    mother: "SANDHYA DEVI",
-    session: "JULY-2022",
-    course: "BACHELOR OF SCIENCE",
-    stream: "PCM (NEP)",
-    category: "GENERAL",
-    gender: "MALE",
-    year: "3",
+    /* =========================
+       ABHISHEK PAIL WAR
+    ========================== */
+    "37652": {
+        name: "ABHISHEK PAIL WAR",
+        father: "MAHAVEER SINGH",
+        mother: "-",
 
-    address: "VILL - TIWARPUR",
-    district: "VARANASI",
-    state: "UTTAR PRADESH",
-    country: "INDIA",
+        session: "2024-25",
+        course: "MASTER OF BUSINESS ADMINISTRATION",
+        stream: "MARKETING & HR",
 
-    photo: "images/rupesh.jpg"
-}
-]
-}
+        category: "OBC",
+        gender: "MALE",
+        year: "1",
+
+        address: "DIGRAUTA, AGRA",
+        district: "AGRA",
+        state: "UTTAR PRADESH",
+        country: "INDIA",
+
+        photo: "images/abhishek.jpg",
+
+        qualification: [
+            {
+                exam: "10th",
+                year: "2012",
+                board: "Raj Board",
+                marks: "50",
+                subjects: "Hindi, English, Maths, Science"
+            },
+            {
+                exam: "12th",
+                year: "2015",
+                board: "UP BOARD",
+                marks: "51",
+                subjects: "Hindi, English, Maths"
+            },
+            {
+                exam: "Graduation",
+                year: "2019",
+                board: "SVSU Meerut",
+                marks: "67",
+                subjects: "Maths / Physics"
+            }
+        ]
+    },
+
+
+    /* =========================
+       RUPESH KUMAR SINGH
+    ========================== */
+    "SRU220740087": {
+        name: "RUPESH KUMAR SINGH",
+        father: "SANTOSH KUMAR SINGH",
+        mother: "SANDHYA DEVI",
+
+        session: "JULY-2022",
+        course: "BACHELOR OF SCIENCE",
+        stream: "PCM (NEP)",
+
+        category: "GENERAL",
+        gender: "MALE",
+        year: "3",
+
+        address: "VILL - TIWARPUR",
+        district: "VARANASI",
+        state: "UTTAR PRADESH",
+        country: "INDIA",
+
+        photo: "images/rupesh.jpg",
+
+        qualification: [
+            {
+                exam: "SECONDARY",
+                year: "2013",
+                board: "UP BOARD",
+                marks: "64",
+                subjects: "HINDI, ENG, MATHS, SCIENCE"
+            },
+            {
+                exam: "SR. SECONDARY",
+                year: "2015",
+                board: "UP BOARD",
+                marks: "56",
+                subjects: "GEN HINDI, ENG, MATHS, PHYSICS"
+            }
+        ]
+    }
+
 };
